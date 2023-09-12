13b8c3ee92b90477806d2bcfaed2f0abbcb9e127
antoniomfa <antoniomiguelferreiraaires@hotmail.com>
2023-09-12 09:41:29 +0100

    Display listings

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -2,12 +2,27 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
 
 export const listingsRouter = createTRPCRouter({
+    list: publicProcedure.query(({ ctx }) => {
+        return ctx.prisma.listing.findMany();
+    }),
+    get: publicProcedure
+        .input(z.object({ listingId: z.string() }))
+        .query(({ ctx, input }) => {
+            console.log(ctx);
+            console.log(input);
+            
+            return ctx.prisma.listing.findUnique({
+                where:  {
+                    id: input.listingId
+                }
+            })
+        }),
     create: protectedProcedure.input(z.object({
         name: z.string(),
         description: z.string(),
         price: z.string()
     })
-    ).mutation(async ({ input, ctx }) => {        
+    ).mutation(async ({ input, ctx }) => {
         const listing = await ctx.prisma.listing.create({
             data: {
                 ...input,
